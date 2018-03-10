afea0b76742ab5f71dc1e76cb0c84171382949b9
jason <jasondepas99@gmail.com>
2018-03-10 13:23:20 -0700

    set up changes

--- visual code/html and javascript intro class/scratch.js
+++ visual code/html and javascript intro class/scratch.js
@@ -1,1 +1,9 @@
-console.log("does it work");
+console.log("hello world testing 123!");
+
+
+let json = "Jason"
+
+document.body.insertAdjacentHTML('afterbegin',`<div>Hi, my name is ${json}<\div>`);
+
+
+
